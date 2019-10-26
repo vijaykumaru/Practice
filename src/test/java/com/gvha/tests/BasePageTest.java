74866092f8670cca862704c3ca900eecad73adae
vijayakumaru@gmail.com <vijaykumaru@gmail.com>
2019-10-26 12:56:28 +0530

    First commit

---
+++ src/test/java/com/gvha/tests/BasePageTest.java
@@ -0,0 +1,62 @@
+package com.gvha.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.gvha.base.ExtentTestManager;
+import com.gvha.base.TestBase;
+import com.gvha.pages.BasePage;
+
+import static com.aventstack.extentreports.Status.INFO;
+
+public class BasePageTest extends TestBase {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = getDriver();
+    }
+
+    @Test
+    public void verifyHomePage() {
+        System.out.println("Home page test...");
+        BasePage basePage = new BasePage(driver);
+        Assert.assertTrue(basePage.verifyBasePageTitle(), "Home page title doesn't match");
+    }
+
+    @Test
+    public void baseTest1() {
+        ExtentTestManager.getTest().log(INFO, "Hellooo started base test1");
+        System.out.println("Hey im in test1 test");
+        ExtentTestManager.getTest().log(INFO, "Hey im in base test1 1");
+        ExtentTestManager.getTest().log(INFO, "Hey im in base test1 2");
+        ExtentTestManager.getTest().log(INFO, "Hey im in base test1 3");
+        ExtentTestManager.getTest().log(INFO, "Hey im in base test1 4");
+    }
+
+    @Test
+    public void baseTest2() throws InterruptedException {
+        ExtentTestManager.getTest().log(INFO, "Hellooo started base test2");
+        System.out.println("Hey im in test2 test");
+        Thread.sleep(3000);
+        ExtentTestManager.getTest().log(INFO, "Hey im in base test2 1");
+        ExtentTestManager.getTest().log(INFO, "Hey im in base test2 2");
+    }
+
+    @Test
+    public void baseTest3() {
+        ExtentTestManager.getTest().log(INFO, "Hellooo started base test3");
+        System.out.println("Hey im in test3 test");
+        ExtentTestManager.getTest().log(INFO, "Hey im in base test3 1");
+        ExtentTestManager.getTest().log(INFO, "Hey im in base test3 2");
+    }
+
+ /*   @AfterClass
+    public void tearDown() {
+
+
+    }*/
+}
---
+++ src/test/java/com/gvha/tests/SignInPageTest.java
@@ -0,0 +1,48 @@
+package com.gvha.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.gvha.base.TestBase;
+import com.gvha.pages.BasePage;
+import com.gvha.pages.SignInPage;
+
+public class SignInPageTest extends TestBase {
+
+    private WebDriver driver;
+    private SignInPage signInPage;
+    private BasePage basePage;
+
+    @BeforeClass
+    public void setUp() {
+        //Get driver
+        driver = getDriver();
+    }
+
+    @Test
+    public void verifySignInFunction() {
+        System.out.println("Sign In functionality details...");
+        basePage = new BasePage(driver);
+        signInPage = basePage.clickSignInBtn();
+        Assert.assertTrue(signInPage.verifySignInPageTitle(), "Sign In page title doesn't match");
+        Assert.assertTrue(signInPage.verifySignInPageText(), "Page text not matching");
+        Assert.assertTrue(signInPage.verifySignIn(), "Unable to sign in");
+    }
+
+    @Test
+    public void SignInPageTestOne() {
+        System.out.println("Hey im in example test One");
+    }
+
+    @Test
+    public void SignInPageTestTwo() {
+        System.out.println("Hey im in example test two");
+    }
+
+    @Test
+    public void SignInPageTestThree() {
+        System.out.println("Hey im in example test three");
+    }
+}
